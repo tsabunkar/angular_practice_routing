6cd95cd04a983906ff65413f6154e0ce9c79e05b
tsabunkar <tsabunkar@gmail.com>
2018-09-09 08:34:42 +0530

    Children routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,16 +10,34 @@ import { ServersComponent } from './servers/servers.component';
 import { ServerComponent } from './servers/server/server.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { HomeComponent } from './home/home.component';
-import { UserComponent } from './users/user/user.component';
 import { ServersService } from './servers/servers.service';
+import { UserComponent } from './users/user/user.component';
 
-const appRoutes: Routes = [ //this Routes is an array , which has list of all the routes
+/* const appRoutes: Routes = [ //this Routes is an array , which has list of all the routes
   { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent },
-  { path: 'users/:myid/:myname', component: UsersComponent }, // myid -> dynamic paramter in the url
+  { path: 'users/:myid/:myname', component: UserComponent }, // myid -> dynamic paramter in the url
   { path: 'servers', component: ServersComponent },
   { path: 'servers/:myIdVara', component: ServerComponent },
   { path: 'servers/:id/editmyserver', component: EditServerComponent },
+] */
+
+//?using the concept of Child Routes
+
+const appRoutes: Routes = [ //this Routes is an array , which has list of all the routes
+  { path: '', component: HomeComponent },
+  {
+    path: 'users', component: UsersComponent, children: [
+      { path: ':myid/:myname', component: UserComponent } //smiliar to ->  { path: 'users/:myid/:myname', component: UsersComponent },
+    ]
+  },
+  {
+    path: 'servers', component: ServersComponent, children: [
+      { path: ':myIdVara', component: ServerComponent },
+      { path: ':id/editmyserver', component: EditServerComponent }
+    ]
+  },
+
 ]
 @NgModule({
   declarations: [
